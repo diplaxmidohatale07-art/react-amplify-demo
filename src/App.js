075ffd2034cec58bb2b2880e1677f25411ef99ba
sanchaysh <shobhanesanchay@gmail.com>
2026-07-21 11:36:05 +0530

    Initial React application

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>🚀 Welcome to My First AWS React Project</h1>
+      <h2>Built by Diplaxmi</h2>
+      <p>I am learning React, Git, GitHub, AWS Amplify and CI/CD.</p>
+    </div>
+  );
+}
+
+export default App;
